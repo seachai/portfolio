44f0bb9b19615264422118df3e8dda03f7e672c2
Chai Lee <imchai@gmail.com>
2019-06-23 21:37:08 -0400

    Added react spring to the mix

--- src/Components/TopSection.js
+++ src/Components/TopSection.js
@@ -1,25 +1,32 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 export default function TopSection() {
+  const animation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 }
+  });
   return (
-    <section id="top">
-      <div className="info">
-        <div className="square" />
-        <h1>Chai Lee</h1>
-        <p>Front End Developer</p>
-        <a href="mailto:imchai@gmail.com" className="hire">
-          Available for Hire
-        </a>
-      </div>
-      <div className="about">
-        <span className="title">About Me</span>
-        <p>
-          Hello my name is Chai. When I first wrote the lines hello world, I was
-          awestruck. I'm a passionate, ambitious and adventurous developer. I
-          spend my time sweating at the gym, gaining weight at restaurants and
-          getting lost in code.
-        </p>
-      </div>
-    </section>
+    <animated.div style={animation}>
+      <section id="top">
+        <div className="info">
+          <div className="square" />
+          <h1>Chai Lee</h1>
+          <p>Front End Developer</p>
+          <a href="mailto:imchai@gmail.com" className="hire">
+            Available for Hire
+          </a>
+        </div>
+        <div className="about">
+          <span className="title">About Me</span>
+          <p>
+            Hello my name is Chai. When I first wrote the lines hello world, I
+            was awestruck. I'm a passionate, ambitious and adventurous
+            developer. I spend my time sweating at the gym, gaining weight at
+            restaurants and getting lost in code.
+          </p>
+        </div>
+      </section>
+    </animated.div>
   );
 }
